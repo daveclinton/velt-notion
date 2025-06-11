c902ba8eb2ca51aa949f6c554a0259be6ff45353
daveclinton <clintondavid46@gmail.com>
2025-06-11 12:05:23 +0300

    feat: save item clerk next js import

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -22,7 +22,7 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
-import { useUser } from "@clerk/clerk-react";
+import { useUser } from "@clerk/nextjs";
 
 interface ItemProps {
   id?: Id<"documents">;
