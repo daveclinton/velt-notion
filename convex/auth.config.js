825e2dddc2ee8c0e1f5a0b3b18069e5da3ea6ec7
daveclinton <clintondavid46@gmail.com>
2025-06-11 13:48:51 +0300

    feat:add auth config

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,8 @@
 export default {
   providers: [
     {
-      domain: "https://choice-eagle-35.clerk.accounts.dev",
+      // domain: "https://choice-eagle-35.clerk.accounts.dev",
+      domain: " https://clerk.velt-notion.daveclintonn.cc",
       applicationID: "convex",
     },
   ],
