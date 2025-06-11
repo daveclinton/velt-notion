c4914a39e97396582e939d54c2d9edda06dd60e4
daveclinton <clintondavid46@gmail.com>
2025-06-11 11:07:29 +0300

    feat: add velt config

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
-    providers: [
-        {
-            domain: "https://meet-polecat-10.clerk.accounts.dev",
-            applicationID: "convex"
-        }
-    ]
-}
+  providers: [
+    {
+      domain: "https://choice-eagle-35.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
