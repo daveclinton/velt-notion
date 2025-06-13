586cb4f38163cb2ffdecb6242e8b80251ec0c4c4
daveclinton <clintondavid46@gmail.com>
2025-06-13 23:01:39 +0300

    feat: add page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Heading from "./_components/heading";
-import Heroes from "./_components/heroes";
 import Footer from "./_components/footer";
 import RecentlyVisited from "@/components/recently-visited";
 
